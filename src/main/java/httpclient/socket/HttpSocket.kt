e1e00b7c6afa6068e81cc2c840215fedf3ea60ba
shiq <shiq@yxt.com>
2019-07-31 06:40:45 +0100

    Update: add socket

--- src/main/java/httpclient/socket/HttpSocket.kt
+++ src/main/java/httpclient/socket/HttpSocket.kt
@@ -2,8 +2,12 @@ package httpclient.socket
 
 import java.net.Socket
 
-class HttpSocket {
+class HttpSocket(urlParams: URLParams) {
 
-    private val socket = Socket()
+    private val socket = Socket(urlParams.ip, urlParams.port)
+
+    fun sendData(data: String?) : String? {
+        return ""
+    }
 
 }
---
+++ src/main/java/httpclient/socket/URLParams.kt
@@ -0,0 +1,8 @@
+package httpclient.socket
+
+class URLParams {
+
+    var ip: String? = ""
+    var port: Int = 0
+
+}
